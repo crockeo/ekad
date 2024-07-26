1c8205c38746e77e2add07df031b64a372cfbbed
Cerek Hillen <cerekh@gmail.com>
2024-07-26 20:40:24 +0200

    reset to vello's example

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,243 @@
+// This is all taken (and eventually modified)
+// from https://github.com/linebender/vello/blob/main/examples/simple/src/main.rs
+
+use anyhow::Result;
 use std::num::NonZeroUsize;
 use std::sync::Arc;
-use std::time::Instant;
+use vello::kurbo::{Affine, Circle, Ellipse, Line, RoundedRect, Stroke};
+use vello::peniko::Color;
+use vello::util::{RenderContext, RenderSurface};
+use vello::wgpu;
+use vello::{AaConfig, Renderer, RendererOptions, Scene};
+use winit::application::ApplicationHandler;
+use winit::dpi::LogicalSize;
+use winit::event::*;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::Window;
 
-fn main() -> anyhow::Result<()> {
-    pretty_env_logger::init();
-    log::info!("Hello world!");
-
-    let event_loop = winit::event_loop::EventLoop::new()?;
-    let render_cx = vello::util::RenderContext::new();
-
-    let mut app = App {
-        render_cx,
-        render_state: None,
-        renderers: Vec::new(),
-        window: None,
-    };
-    event_loop.run_app(&mut app)?;
-
-    Ok(())
+// Simple struct to hold the state of the renderer
+pub struct ActiveRenderState<'s> {
+    // The fields MUST be in this order, so that the surface is dropped before the window
+    surface: RenderSurface<'s>,
+    window: Arc<Window>,
 }
 
-const AA_CONFIGS: [vello::AaConfig; 3] = [
-    vello::AaConfig::Area,
-    vello::AaConfig::Msaa8,
-    vello::AaConfig::Msaa16,
-];
-
-struct RenderState<'a> {
-    // SAFETY: We MUST drop the surface before the `window`, so the fields
-    // must be in this order
-    surface: vello::util::RenderSurface<'a>,
-    window: Arc<winit::window::Window>,
+enum RenderState<'s> {
+    Active(ActiveRenderState<'s>),
+    // Cache a window so that it can be reused when the app is resumed after being suspended
+    Suspended(Option<Arc<Window>>),
 }
 
-struct App<'a> {
-    render_cx: vello::util::RenderContext,
-    render_state: Option<RenderState<'a>>,
-    renderers: Vec<Option<vello::Renderer>>,
+struct SimpleVelloApp<'s> {
+    // The vello RenderContext which is a global context that lasts for the
+    // lifetime of the application
+    context: RenderContext,
 
-    window: Option<Arc<winit::window::Window>>,
+    // An array of renderers, one per wgpu device
+    renderers: Vec<Option<Renderer>>,
+
+    // State for our example where we store the winit Window and the wgpu Surface
+    state: RenderState<'s>,
+
+    // A vello Scene which is a data structure which allows one to build up a
+    // description a scene to be drawn (with paths, fills, images, text, etc)
+    // which is then passed to a renderer for rendering
+    scene: Scene,
 }
 
-impl<'a> winit::application::ApplicationHandler for App<'a> {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        // The majority of this is copied and modified from Vello's `with_winit` example:
-        // https://github.com/linebender/vello/blob/main/examples/with_winit/src/lib.rs
-
-        if !self.render_state.is_none() {
+impl<'s> ApplicationHandler for SimpleVelloApp<'s> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let RenderState::Suspended(cached_window) = &mut self.state else {
             return;
-        }
+        };
 
-        let window = self.window.take().unwrap_or_else(|| {
-            Arc::new(
-                event_loop
-                    .create_window(winit::window::Window::default_attributes())
-                    .unwrap(),
-            )
-        });
+        // Get the winit window cached in a previous Suspended event or else create a new window
+        let window = cached_window
+            .take()
+            .unwrap_or_else(|| create_winit_window(event_loop));
 
+        // Create a vello Surface
         let size = window.inner_size();
-        let surface_future = self.render_cx.create_surface(
+        let surface_future = self.context.create_surface(
             window.clone(),
             size.width,
             size.height,
-            vello::wgpu::PresentMode::AutoVsync,
+            wgpu::PresentMode::AutoVsync,
         );
-        // We need to block here, in case a Suspended event appeared
         let surface = pollster::block_on(surface_future).expect("Error creating surface");
-        self.render_state = {
-            let render_state = RenderState { window, surface };
-            self.renderers
-                .resize_with(self.render_cx.devices.len(), || None);
-            let id = render_state.surface.dev_id;
-            self.renderers[id].get_or_insert_with(|| {
-                let start = Instant::now();
-                #[allow(unused_mut)]
-                let mut renderer = vello::Renderer::new(
-                    &self.render_cx.devices[id].device,
-                    vello::RendererOptions {
-                        surface_format: Some(render_state.surface.format),
-                        use_cpu: false,
-                        antialiasing_support: AA_CONFIGS.iter().copied().collect(),
-                        num_init_threads: NonZeroUsize::new(2), // TODO: what should this number be?
-                    },
-                )
-                .map_err(|e| {
-                    // Pretty-print any renderer creation error using Display formatting before unwrapping.
-                    anyhow::format_err!("{e}")
-                })
-                .expect("Failed to create renderer");
-                log::info!("Creating renderer {id} took {:?}", start.elapsed());
-                renderer
-            });
-            Some(render_state)
-        };
-        event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+
+        // Create a vello Renderer for the surface (using its device id)
+        self.renderers
+            .resize_with(self.context.devices.len(), || None);
+        self.renderers[surface.dev_id]
+            .get_or_insert_with(|| create_vello_renderer(&self.context, &surface));
+
+        // Save the Window and Surface to a state variable
+        self.state = RenderState::Active(ActiveRenderState { window, surface });
+
+        event_loop.set_control_flow(ControlFlow::Poll);
     }
 
-    fn suspended(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        log::info!("Suspending");
-        // When we suspend, we need to remove the `wgpu` Surface
-        if let Some(render_state) = self.render_state.take() {
-            self.window = Some(render_state.window);
+    fn suspended(&mut self, event_loop: &ActiveEventLoop) {
+        if let RenderState::Active(state) = &self.state {
+            self.state = RenderState::Suspended(Some(state.window.clone()));
         }
-        event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
+        event_loop.set_control_flow(ControlFlow::Wait);
     }
 
     fn window_event(
         &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
+        event_loop: &ActiveEventLoop,
         window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
+        event: WindowEvent,
     ) {
-        use winit::event::WindowEvent::*;
+        // Ignore the event (return from the function) if
+        //   - we have no render_state
+        //   - OR the window id of the event doesn't match the window id of our render_state
+        //
+        // Else extract a mutable reference to the render state from its containing option for use below
+        let render_state = match &mut self.state {
+            RenderState::Active(state) if state.window.id() == window_id => state,
+            _ => return,
+        };
+
         match event {
-            CloseRequested => event_loop.exit(),
+            // Exit the event loop when a close is requested (e.g. window's close button is pressed)
+            WindowEvent::CloseRequested => event_loop.exit(),
 
-            RedrawRequested => {
-                let Some(RenderState { surface, window }) = &self.render_state else {
-                    return;
-                };
-
-                // TODO: render!
+            // Resize the surface when the window is resized
+            WindowEvent::Resized(size) => {
+                self.context
+                    .resize_surface(&mut render_state.surface, size.width, size.height);
+                render_state.window.request_redraw();
             }
 
-            Resized(size) => {
-                if let Some(RenderState { surface, window }) = &mut self.render_state {
-                    self.render_cx
-                        .resize_surface(surface, size.width, size.height);
-                    window.request_redraw();
-                }
-            }
+            // This is where all the rendering happens
+            WindowEvent::RedrawRequested => {
+                // Empty the scene of objects to draw. You could create a new Scene each time, but in this case
+                // the same Scene is reused so that the underlying memory allocation can also be reused.
+                self.scene.reset();
 
+                // Re-add the objects to draw to the scene.
+                add_shapes_to_scene(&mut self.scene);
+
+                // Get the RenderSurface (surface + config)
+                let surface = &render_state.surface;
+
+                // Get the window size
+                let width = surface.config.width;
+                let height = surface.config.height;
+
+                // Get a handle to the device
+                let device_handle = &self.context.devices[surface.dev_id];
+
+                // Get the surface's texture
+                let surface_texture = surface
+                    .surface
+                    .get_current_texture()
+                    .expect("failed to get surface texture");
+
+                // Render to the surface's texture
+                self.renderers[surface.dev_id]
+                    .as_mut()
+                    .unwrap()
+                    .render_to_surface(
+                        &device_handle.device,
+                        &device_handle.queue,
+                        &self.scene,
+                        &surface_texture,
+                        &vello::RenderParams {
+                            base_color: Color::BLACK, // Background color
+                            width,
+                            height,
+                            antialiasing_method: AaConfig::Msaa16,
+                        },
+                    )
+                    .expect("failed to render to surface");
+
+                // Queue the texture to be presented on the surface
+                surface_texture.present();
+
+                device_handle.device.poll(wgpu::Maintain::Poll);
+            }
             _ => {}
         }
     }
+}
+
+fn main() -> Result<()> {
+    // Setup a bunch of state:
+    let mut app = SimpleVelloApp {
+        context: RenderContext::new(),
+        renderers: vec![],
+        state: RenderState::Suspended(None),
+        scene: Scene::new(),
+    };
+
+    // Create and run a winit event loop
+    let event_loop = EventLoop::new()?;
+    event_loop
+        .run_app(&mut app)
+        .expect("Couldn't run event loop");
+    Ok(())
+}
+
+/// Helper function that creates a Winit window and returns it (wrapped in an Arc for sharing between threads)
+fn create_winit_window(event_loop: &ActiveEventLoop) -> Arc<Window> {
+    let attr = Window::default_attributes()
+        .with_inner_size(LogicalSize::new(1044, 800))
+        .with_resizable(true)
+        .with_title("Vello Shapes");
+    Arc::new(event_loop.create_window(attr).unwrap())
+}
+
+/// Helper function that creates a vello `Renderer` for a given `RenderContext` and `RenderSurface`
+fn create_vello_renderer(render_cx: &RenderContext, surface: &RenderSurface) -> Renderer {
+    Renderer::new(
+        &render_cx.devices[surface.dev_id].device,
+        RendererOptions {
+            surface_format: Some(surface.format),
+            use_cpu: false,
+            antialiasing_support: vello::AaSupport::all(),
+            num_init_threads: NonZeroUsize::new(1),
+        },
+    )
+    .expect("Couldn't create renderer")
+}
+
+/// Add shapes to a vello scene. This does not actually render the shapes, but adds them
+/// to the Scene data structure which represents a set of objects to draw.
+fn add_shapes_to_scene(scene: &mut Scene) {
+    // Draw an outlined rectangle
+    let stroke = Stroke::new(6.0);
+    let rect = RoundedRect::new(10.0, 10.0, 240.0, 240.0, 20.0);
+    let rect_stroke_color = Color::rgb(0.9804, 0.702, 0.5294);
+    scene.stroke(&stroke, Affine::IDENTITY, rect_stroke_color, None, &rect);
+
+    // Draw a filled circle
+    let circle = Circle::new((420.0, 200.0), 120.0);
+    let circle_fill_color = Color::rgb(0.9529, 0.5451, 0.6588);
+    scene.fill(
+        vello::peniko::Fill::NonZero,
+        Affine::IDENTITY,
+        circle_fill_color,
+        None,
+        &circle,
+    );
+
+    // Draw a filled ellipse
+    let ellipse = Ellipse::new((250.0, 420.0), (100.0, 160.0), -90.0);
+    let ellipse_fill_color = Color::rgb(0.7961, 0.651, 0.9686);
+    scene.fill(
+        vello::peniko::Fill::NonZero,
+        Affine::IDENTITY,
+        ellipse_fill_color,
+        None,
+        &ellipse,
+    );
+
+    // Draw a straight line
+    let line = Line::new((260.0, 20.0), (620.0, 100.0));
+    let line_stroke_color = Color::rgb(0.5373, 0.7059, 0.9804);
+    scene.stroke(&stroke, Affine::IDENTITY, line_stroke_color, None, &line);
 }
