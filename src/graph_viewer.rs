e2a58a60d1b0778c713ce56e1c94b8b37e2211ff
Cerek Hillen <cerekh@gmail.com>
2024-07-30 11:03:44 +0200

    basic gesture system, previews

--- src/graph_viewer.rs
+++ src/graph_viewer.rs
@@ -27,19 +27,26 @@ const LIGHT_COLOR: Color = Color {
     b: 222,
     a: 255,
 };
+const PREVIEW_COLOR: Color = Color {
+    r: 113,
+    g: 64,
+    b: 237,
+    a: 127,
+};
 
 #[derive(Default)]
 pub struct GraphViewer {
+    gesture: Gesture,
     graph: DiGraph<Circle, ()>,
     raw_mouse_position: Option<Point>,
-    // TODO: generalize this into some kind of gesture system
-    start_circle: Option<NodeIndex<u32>>,
     transform: Affine,
 }
 
 impl GraphViewer {
     pub fn add_to_scene(&self, parent_scene: &mut Scene) {
         let mut scene = Scene::new();
+
+        let line_stroke = Stroke::new(2.0);
         for circle_id in self.graph.node_indices() {
             let circle = &self.graph[circle_id];
 
@@ -61,68 +68,99 @@ impl GraphViewer {
                 None,
                 circle,
             );
-
-            let stroke = Stroke::new(2.0);
             for neighbor_circle_id in self.graph.neighbors(circle_id) {
                 let neighbor_circle = &self.graph[neighbor_circle_id];
                 let line = Line::new(circle.center, neighbor_circle.center);
-                scene.stroke(&stroke, Affine::IDENTITY, BASE_COLOR, None, &line);
+                scene.stroke(&line_stroke, Affine::IDENTITY, BASE_COLOR, None, &line);
             }
         }
 
-        // TODO: add a debug mode which will let us turn this off and on?
-        // if let Some(mouse_position) = self.mouse_position() {
-        //     let mouse_pos_circle = Circle::new(mouse_position, 10.0);
-        //     scene.fill(
-        //         vello::peniko::Fill::NonZero,
-        //         Affine::IDENTITY,
-        //         BASE_COLOR,
-        //         None,
-        //         &mouse_pos_circle,
-        //     );
-        // };
+        match (self.mouse_position(), self.gesture, self.hovered_circle()) {
+            (None, _, _) => {}
+            (_, Gesture::Inactive, _) => {}
+            (_, Gesture::AddingNode, Some(_)) => {}
+            (Some(mouse_position), Gesture::AddingNode, None) => {
+                scene.fill(
+                    vello::peniko::Fill::NonZero,
+                    Affine::IDENTITY,
+                    PREVIEW_COLOR,
+                    None,
+                    &Circle::new(mouse_position, 40.0),
+                );
+            }
+            (Some(mouse_position), Gesture::AddingEdge { from }, None) => {
+                scene.fill(
+                    vello::peniko::Fill::NonZero,
+                    Affine::IDENTITY,
+                    PREVIEW_COLOR,
+                    None,
+                    &Circle::new(mouse_position, 40.0),
+                );
+                scene.stroke(
+                    &line_stroke,
+                    Affine::IDENTITY,
+                    PREVIEW_COLOR,
+                    None,
+                    &Line::new(self.graph[from].center, mouse_position),
+                );
+            }
+            (_, Gesture::AddingEdge { from }, Some(to)) => {
+                scene.stroke(
+                    &line_stroke,
+                    Affine::IDENTITY,
+                    PREVIEW_COLOR,
+                    None,
+                    &Line::new(self.graph[from].center, self.graph[to].center),
+                );
+            }
+        }
 
         parent_scene.append(&scene, Some(self.transform));
     }
 
     pub fn mouse_moved(&mut self, new_position: Option<Point>) -> bool {
-        // let circle = Circle::new((420.0, 200.0), 120.0);
-        // let new_position = Point::new(position.x, position.y);
-        // let changed_hover =
-        //     in_circle(&self.mouse_position, &circle) != in_circle(&new_position, &circle);
-        // self.mouse_position = new_position;
-        // if changed_hover {
-        //     render_state.window.request_redraw();
-        // }
         self.raw_mouse_position = new_position;
+
+        // TODO: don't redraw the screen every time one moves the mouse,
+        // only when it would result in something needing a redraw
         true
     }
 
-    // TODO: draw preview when
-    // - making a new circle
-    // - making a new connection
-    // - making a new circle with a new connection at the same time
     pub fn mouse_pressed(&mut self) {
+        let Gesture::Inactive = self.gesture else {
+            return;
+        };
+
         if let Some(circle) = self.hovered_circle() {
-            self.start_circle = Some(circle);
+            self.gesture = Gesture::AddingEdge { from: circle };
+        } else {
+            self.gesture = Gesture::AddingNode;
         }
     }
 
     pub fn mouse_released(&mut self) {
-        let target_circle = if let Some(circle_id) = self.hovered_circle() {
-            circle_id
-        } else {
-            let Some(mouse_position) = self.mouse_position() else {
-                return;
-            };
-            self.graph
-                .add_node(Circle::new(mouse_position.clone(), 40.0))
-        };
+        let hovered_circle = self.hovered_circle();
+        let mouse_position = self.mouse_position();
 
-        if let Some(start_circle) = self.start_circle {
-            self.graph.add_edge(start_circle, target_circle, ());
+        match (self.gesture, hovered_circle) {
+            (Gesture::Inactive, _) => {}
+            (Gesture::AddingNode, Some(_)) => {}
+            (Gesture::AddingNode, None) => {
+                if let Some(mouse_position) = mouse_position {
+                    self.graph.add_node(Circle::new(mouse_position, 40.0));
+                }
+            }
+            (Gesture::AddingEdge { from }, None) => {
+                if let Some(mouse_position) = mouse_position {
+                    let to = self.graph.add_node(Circle::new(mouse_position, 40.0));
+                    self.graph.add_edge(from, to, ());
+                }
+            }
+            (Gesture::AddingEdge { from }, Some(to)) => {
+                self.graph.add_edge(from, to, ());
+            }
         }
-        self.start_circle = None;
+        self.gesture = Gesture::Inactive;
     }
 
     pub fn scroll(&mut self, delta_x: f64, delta_y: f64) {
@@ -168,48 +206,19 @@ impl GraphViewer {
     }
 }
 
-// TODO: example
-// /// Add shapes to a vello scene. This does not actually render the shapes, but adds them
-// /// to the Scene data structure which represents a set of objects to draw.
-// fn add_shapes_to_scene(mouse_position: &Point, scene: &mut Scene) {
-//     // Draw an outlined rectangle
-//     let stroke = Stroke::new(6.0);
-//     let rect = RoundedRect::new(10.0, 10.0, 240.0, 240.0, 20.0);
-//     let rect_stroke_color = Color::rgb(0.9804, 0.702, 0.5294);
-//     scene.stroke(&stroke, Affine::IDENTITY, rect_stroke_color, None, &rect);
-
-//     // Draw a filled circle
-//     let circle = Circle::new((420.0, 200.0), 120.0);
-//     let circle_fill_color = if in_circle(mouse_position, &circle) {
-//         Color::rgb(0.9529, 0.5451, 0.6588)
-//     } else {
-//         Color::rgb8(210, 123, 53)
-//     };
-//     scene.fill(
-//         vello::peniko::Fill::NonZero,
-//         Affine::IDENTITY,
-//         circle_fill_color,
-//         None,
-//         &circle,
-//     );
-
-//     // Draw a filled ellipse
-//     let ellipse = Ellipse::new((250.0, 420.0), (100.0, 160.0), -90.0);
-//     let ellipse_fill_color = Color::rgb(0.7961, 0.651, 0.9686);
-//     scene.fill(
-//         vello::peniko::Fill::NonZero,
-//         Affine::IDENTITY,
-//         ellipse_fill_color,
-//         None,
-//         &ellipse,
-//     );
-
-//     // Draw a straight line
-//     let line = Line::new((260.0, 20.0), (620.0, 100.0));
-//     let line_stroke_color = Color::rgb(0.5373, 0.7059, 0.9804);
-//     scene.stroke(&stroke, Affine::IDENTITY, line_stroke_color, None, &line);
-// }
-
 fn in_circle(point: &Point, circle: &Circle) -> bool {
     point.distance_squared(circle.center) < circle.radius * circle.radius
+}
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+enum Gesture {
+    Inactive,
+    AddingNode,
+    AddingEdge { from: NodeIndex<u32> },
+}
+
+impl Default for Gesture {
+    fn default() -> Self {
+        Gesture::Inactive
+    }
 }
