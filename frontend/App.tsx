6509896be0107cc38e8bcf9a886fc53dd149b9b5
Cerek Hillen <cerekh@gmail.com>
2024-08-16 18:30:26 -0700

    mess around with adding stuff to the UI

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,120 +1,76 @@
-import ForceGraph, {
-  LinkObject as ForceGraphLinkObject,
-  NodeObject as ForceGraphNodeObject,
-} from "force-graph";
-import React, { useEffect, useRef } from "react";
+import { useState, type FormEvent } from "react";
+import GraphView, { GraphData, type NodeObject } from "./components/GraphView";
+import styled from "styled-components";
 
-interface NodeObject extends ForceGraphNodeObject {
-  nodeColor?: string;
-}
-
-interface LinkObject extends ForceGraphLinkObject {}
-
-interface GraphData {
-  nodes: NodeObject[];
-  links: LinkObject[];
-}
-
-interface GraphViewProps {
-	data: GraphData;
-}
-
-function GraphView({ data }: GraphViewProps) {
-	const ref = useRef<HTMLDivElement>(null);
-	useEffect(() => {
-		if (!ref.current) { return; }
-
-		const graph = ForceGraph()
-		  .enableZoomInteraction(false)
-		  .graphData(data)
-		  .linkDirectionalArrowLength(5)
-		  .linkDirectionalArrowRelPos(1)
-		  .nodeColor((node: NodeObject) => node.nodeColor || "black");
-		graph(ref.current);
-
-		function onWheel(event: WheelEvent) {
-			if (!ref.current) { return; }
-
-			event.preventDefault();
-
-			let zoom = graph.zoom();
-			let { x, y } = graph.centerAt();
-			if (event.ctrlKey) {
-				// Magic: macOS and Windows laptops with zoom
-				// set `WheelEvent.ctrlKey = true`
-				// when it's a pinch event, and false otherwise.
-				zoom -= (event.deltaY / 100) * zoom;
-				if (zoom < 0.0) {
-					zoom = 0.0;
-				}
-
-				// TODO: Fix this to zoom in on the mouse position.
-				const rect = ref.current.getBoundingClientRect();
-				const rectCenterX = rect.x + rect.width / 2;
-				const rectCenterY = rect.y + rect.height / 2;
-
-				const mouseDeltaX = event.clientX - rectCenterX;
-				const mouseDeltaY = event.clientY - rectCenterY;
-
-				x -= mouseDeltaX * event.deltaY / 100 / zoom;
-				y -= mouseDeltaY * event.deltaY / 100 / zoom;
-			} else {
-				x += event.deltaX / zoom;
-				y += event.deltaY / zoom;
-			}
-			graph.zoom(zoom);
-			graph.centerAt(x, y);
-		}
-
-		const resizeObserver = new ResizeObserver(() => {
-		  if (!ref.current) {
-			  return;
-		  }
-		  graph.width(ref.current.clientWidth);
-		  graph.height(ref.current.clientHeight);
-		});
-
-		ref.current.addEventListener("wheel", onWheel);
-		resizeObserver.observe(ref.current);
-
-		return () => {
-		    if (!ref.current) {
-		        return;
-		    }
-			ref.current.removeEventListener("wheel", onWheel);
-			resizeObserver.disconnect();
-		};
-	}, []);
-
-	return (
-		<div ref={ref} />
-	);
+interface Task {
+  title: string;
+  completedAt: Date | null;
 }
 
 export default function App() {
-	return (
-		<div>
-			Hello world!
-			<GraphView
-				data={{
-					nodes: [
-						{
-							id: "node1",
-							nodeColor: "red",
-						},
-						{
-							id: "node2",
-							nodeColor: "blue",
-						},
-					],
-					links: [
-						{
-							source: "node1",
-							target: "node2",
-						},
-					],
-				}}
-			/>
-		</div>
-	);
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [graphData, setGraphData] = useState<GraphData>({ nodes: [], links: [] });
+
+  return (
+    <AppContainer>
+      <TaskBar>
+        <form onSubmit={newTask}>
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            type="text"
+            value={title}
+          />
+          <button disabled={!title}>Add</button>
+        </form>
+
+        <ul>
+          {tasks.map(task =>
+            <li>{task.title}</li>)
+          }
+        </ul>
+      </TaskBar>
+
+      <GraphView data={graphData} />
+    </AppContainer>
+  );
+
+  function nodeFromTask(task: Task): NodeObject {
+    return {
+      id: task.title,
+    };
+  }
+
+  function newTask(e: FormEvent) {
+    e.preventDefault();
+
+    const newTasks = [
+      ...tasks,
+      {
+        title: title,
+        completedAt: null,
+      },
+    ];
+    
+    setTasks(newTasks);
+    setTitle("");
+    setGraphData({
+      nodes: newTasks.map(nodeFromTask),
+      links: [],
+    });
+  }
 }
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TaskBar = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const GraphContainer = styled.div`
+`;
---
+++ frontend/components/GraphView.tsx
@@ -0,0 +1,115 @@
+import ForceGraph, {
+  type LinkObject as ForceGraphLinkObject,
+  type NodeObject as ForceGraphNodeObject,
+} from "force-graph";
+import { useEffect, useRef } from "react";
+
+export interface NodeObject extends ForceGraphNodeObject {
+  nodeColor?: string;
+}
+
+export interface LinkObject extends ForceGraphLinkObject {}
+
+export interface GraphData {
+  nodes: NodeObject[];
+  links: LinkObject[];
+}
+
+interface GraphViewProps {
+  data: GraphData;
+  onBackgroundClick?: (event: MouseEvent) => void;
+}
+
+export default function GraphView({
+	data,
+	onBackgroundClick
+}: GraphViewProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!ref.current) { return; }
+
+    const graph = ForceGraph()
+      .enableZoomInteraction(false)
+      .graphData(data)
+      .nodeCanvasObjectMode(() => "after")
+      .nodeCanvasObject((node, ctx, globalScale) => {
+        // TODO: this is a strange type assertion to have to do.
+        // how can I pass down the fact that the node here
+        // must in fact be *our* NodeObject,
+        // and not the one from force-graph?
+        if (!node.id || typeof node.id != "string" || !node.x || !node.y) {
+          return;
+        }
+
+        const fontSize = 12 / globalScale;
+        ctx.font = `${fontSize}px Sans-Serif`;
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(node.id.replace(/^suma\.apps\./, ""), node.x, node.y + 6);
+      })
+      .onBackgroundClick((evt) => onBackgroundClick && onBackgroundClick(evt))
+      .linkDirectionalArrowLength(5)
+      .linkDirectionalArrowRelPos(1)
+      .nodeColor((node: NodeObject) => node.nodeColor || "black");
+    graph(ref.current);
+
+    function onWheel(event: WheelEvent) {
+      if (!ref.current) { return; }
+
+      event.preventDefault();
+
+      let zoom = graph.zoom();
+      let { x, y } = graph.centerAt();
+      if (event.ctrlKey) {
+        // Magic: macOS and Windows laptops with zoom
+        // set `WheelEvent.ctrlKey = true`
+        // when it's a pinch event, and false otherwise.
+        zoom -= (event.deltaY / 100) * zoom;
+        if (zoom < 0.0) {
+          zoom = 0.0;
+        }
+
+        // When you zoom in in-world mouse position gets translated.
+        // This code adjusts the center of the graph
+        // such that the mouse position in the world remains the same.
+        const rect = ref.current.getBoundingClientRect();
+        const rectCenterX = rect.x + rect.width / 2;
+        const rectCenterY = rect.y + rect.height / 2;
+
+        const mouseDeltaX = event.clientX - rectCenterX;
+        const mouseDeltaY = event.clientY - rectCenterY;
+
+        x -= mouseDeltaX * event.deltaY / 100 / zoom;
+        y -= mouseDeltaY * event.deltaY / 100 / zoom;
+      } else {
+        x += event.deltaX / zoom;
+        y += event.deltaY / zoom;
+      }
+      graph.zoom(zoom);
+      graph.centerAt(x, y);
+    }
+
+    const resizeObserver = new ResizeObserver(() => {
+      if (!ref.current) {
+        return;
+      }
+      graph.width(ref.current.clientWidth);
+      graph.height(ref.current.clientHeight);
+    });
+
+    ref.current.addEventListener("wheel", onWheel);
+    resizeObserver.observe(ref.current);
+
+    return () => {
+        if (!ref.current) {
+            return;
+        }
+      ref.current.removeEventListener("wheel", onWheel);
+      resizeObserver.disconnect();
+    };
+  }, [data]);
+
+  return (
+    <div ref={ref} />
+  );
+}
