6f6d392a1a0dda3bc1c90420d9e44c51fa176877
Cerek Hillen <cerekh@gmail.com>
2024-08-25 18:19:58 -0700

    rehabilitate the modal component

--- frontend/components/Modal.tsx
+++ frontend/components/Modal.tsx
@@ -1,5 +1,5 @@
+import classNames from "classnames";
 import { type PropsWithChildren } from "react";
-import styled from "styled-components";
 
 interface ModalProps {
   onRequestClose: () => void;
@@ -14,33 +14,36 @@ export default function Modal({
   return (
     <>
       {open && (
-        <BackgroundPane onClick={() => onRequestClose()} open={open}>
-          <ForegroundPane onClick={(e) => e.preventDefault()}>
+        <div
+          className={classNames(
+            "bg-black/25",
+            "fixed",
+            "h-screen",
+            "items-center",
+            "justify-center",
+            "left-0",
+            "top-0",
+            "w-screen",
+            "z-50",
+            {
+              flex: open,
+              "display-none": !open,
+            },
+          )}
+          onClick={() => onRequestClose()}
+        >
+          <div
+            className="
+            bg-white
+            rounded
+            p-2
+            "
+            onClick={(e) => e.stopPropagation()}
+          >
             {children}
-          </ForegroundPane>
-        </BackgroundPane>
+          </div>
+        </div>
       )}
     </>
   );
 }
-
-const BackgroundPane = styled.div<{ open: boolean }>`
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.1);
-  display: ${(props) => (props.open ? "flex" : "none")};
-  height: 100%;
-  justify-content: center;
-  left: 0;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 1000;
-`;
-
-// TODO: make this styled with the theme
-const ForegroundPane = styled.div`
-  background-color: #ffffff;
-  border-radius: 1rem;
-  box-shadow: 0px 4px 8px rgb(0, 0, 0, 0.2);
-  padding: 1rem;
-`;
