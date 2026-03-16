3649445d84c290b385675f705bae6f351c052d19
Cerek Hillen <cerekh@gmail.com>
2026-03-15 17:16:11 -0700

    grid layout

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ mod text;
 use crate::graph_viewer::graph_viewer;
 use winit::error::EventLoopError;
 use xilem::{
-    style::Style,
-    view::{flex, label, Axis, FlexExt},
-    Color, EventLoop, WidgetView, WindowOptions, Xilem,
+    view::{flex, grid, label, Axis, GridExt, GridParams},
+    EventLoop, WidgetView, WindowOptions, Xilem,
 };
 
 #[derive(Default)]
@@ -16,29 +15,46 @@ struct AppState {}
 
 impl AppState {
     fn main(&mut self) -> impl WidgetView<AppState> {
-        flex(
-            Axis::Horizontal,
+        grid(
             (
-                flex(Axis::Vertical, label("This is where the menu will go"))
-                    .border(Color::from_rgb8(255, 255, 255), 1.0)
-                    .flex(1.0),
-                (flex(
-                    Axis::Vertical,
-                    (
-                        label("This is where the breadcrumbs will go"),
-                        graph_viewer().flex(1.0),
-                    ),
-                )
-                .border(Color::from_rgb8(255, 255, 255), 1.0)
-                .flex(1.0)),
-                flex(
-                    Axis::Vertical,
-                    label("This is where the node editor will go"),
-                )
-                .border(Color::from_rgb8(255, 255, 255), 1.0)
-                .flex(1.0),
+                self.menu_pane().grid_item(GridParams::new(0, 0, 1, 1)),
+                self.content_pane().grid_item(GridParams::new(1, 0, 4, 1)),
             ),
+            5,
+            1,
         )
+        //
+        // flex(
+        //     Axis::Horizontal,
+        //     (
+        //         flex(Axis::Vertical, label("This is where the menu will go"))
+        //             .border(Color::from_rgb8(255, 255, 255), 1.0)
+        //             .flex(1.0),
+        //         (flex(
+        //             Axis::Vertical,
+        //             (
+        //                 label("This is where the breadcrumbs will go"),
+        //                 graph_viewer().flex(1.0),
+        //             ),
+        //         )
+        //         .border(Color::from_rgb8(255, 255, 255), 1.0)
+        //         .flex(1.0)),
+        //         flex(
+        //             Axis::Vertical,
+        //             label("This is where the node editor will go"),
+        //         )
+        //         .border(Color::from_rgb8(255, 255, 255), 1.0)
+        //         .flex(1.0),
+        //     ),
+        // )
+    }
+
+    fn menu_pane(&mut self) -> impl WidgetView<AppState> {
+        flex(Axis::Vertical, label("Menu pane"))
+    }
+
+    fn content_pane(&mut self) -> impl WidgetView<AppState> {
+        graph_viewer()
     }
 }
 
